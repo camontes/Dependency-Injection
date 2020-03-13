3786e5d7cb705a7fc7bbdd1b29c7fde2b083fa90
juan carlos gonzales montes <pope624@hotmail.com>
2020-03-13 14:24:21 -0500

    Create instance in Main

--- Dependency-Injection/Program.cs
+++ Dependency-Injection/Program.cs
@@ -6,7 +6,13 @@ namespace Dependency_Injection
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Soldier soldier1 = new Soldier(new Gun());
+            Soldier soldier2 = new Soldier(new Rifle());
+            Soldier soldier3 = new Soldier(new Shotgun());
+
+            Console.WriteLine(soldier1.Shoot());
+            Console.WriteLine(soldier2.Shoot());
+            Console.WriteLine(soldier3.Shoot());
         }
     }
 }
